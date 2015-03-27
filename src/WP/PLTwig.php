370ca300c69a11e01cb2fa114747e78184387ada
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-03-27 10:22:03 +0100

    +PLTwig

---
+++ src/WP/PLTwig.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Hospitalplugin\WP;
+
+use Hospitalplugin\Twig\EscapePLCharsExtension;
+
+class PLTwig {
+	
+	public static function load() {
+		if (! class_exists ( 'Twig_Loader_Filesystem' )) {
+			echo 'Twig not activated. Make sure you activate the plugin in
+    <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
+			return;
+		}
+		\Twig_Autoloader::register ();
+		try {
+			$loader = new \Twig_Loader_Filesystem ( __DIR__ . '/../views/' );
+			$twig = new \Twig_Environment ( $loader, array () );
+			// 'debug' => true
+			// 'cache' => '/tmp/'
+			$twig->getExtension ( 'core' )->setTimezone ( 'Europe/Warsaw' ); //
+			$twig->addExtension ( new EscapePLCharsExtension () );
+			return $twig;
+		} catch ( Exception $e ) {
+			echo "ERR: " . $e;
+		}
+	}
+	
+}
