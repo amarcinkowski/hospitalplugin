44f20a0c6b7652f15e9228f26b1a283617cd0fb9
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-24 13:54:29 +0100

    bootstrap paths 4 doctrine

--- src/DB/DoctrineBootstrap.php
+++ src/DB/DoctrineBootstrap.php
@@ -32,93 +32,88 @@ use Doctrine\ORM\Tools\Console\ConsoleRunner;
 /**
  * DoctrineBootstrap
  *
- * @category  Wp
- * @package   Punction
- * @author    Andrzej Marcinkowski <andrzej.max.marcinkowski@gmail.com>
+ * @category Wp
+ * @package Punction
+ * @author Andrzej Marcinkowski <andrzej.max.marcinkowski@gmail.com>
  * @copyright 2014 Wojewódzki Szpital Zespolony, Kalisz
- * @license   MIT http://opensource.org/licenses/MIT
- * @version   1.0 $Id$ $Format:%H$
- * @link      http://
- * @since     File available since Release 1.0.0
+ * @license MIT http://opensource.org/licenses/MIT
+ * @version 1.0 $Id$ $Format:%H$
+ * @link http://
+ * @since File available since Release 1.0.0
  *       
  */
-class DoctrineBootstrap
-{
-
-    /**
-     * entity manager
-     *
-     * @var _entityManager EnitytManager
-     */
-    private static $_entityManager;
-
-    /**
-     * loading envs
-     */
-    private static function _loadEnv()
-    {
-        if (getenv('DB_NAME') != null && getenv('DB_NAME') != "") {
-            return;
-        }
-        if (defined('ABSPATH')) {
-            \Dotenv::load(ABSPATH);
-        } else {
-            \Dotenv::load(getenv('HOME'));
-        }
-    }
-
-    private static function getPaths()
-    {
-        return array(
-            getcwd() . "/src/Entities"
-        );
-    }
-
-    /**
-     *
-     * @return entityManager EntityManager
-     */
-    private static function _getInstance()
-    {
-        $isDevMode = true;
-        $config = Setup::createAnnotationMetadataConfiguration(self::getPaths(), $isDevMode);
-        self::_loadEnv();
-        $conn = array(
-            'dbname' => getenv('DB_NAME'),
-            'user' => getenv('DB_USER'),
-            'password' => getenv('DB_PASSWORD'),
-            'host' => getenv('DB_HOST'),
-            'driver' => 'pdo_mysql',
-            'charset' => 'utf8',
-            'driverOptions' => array(
-                1002 => 'SET NAMES utf8'
-            ),
-            'mapping_types' => 'enum:string'
-        )
-        ;
-        return EntityManager::create($conn, $config);
-    }
-
-    /**
-     * returns enitytManager
-     *
-     * @return EntityManager
-     */
-    public static function getEntityManager()
-    {
-        if (self::$_entityManager == null) {
-            self::$_entityManager = self::_getInstance();
-        }
-        return self::$_entityManager;
-    }
-
-    public static function getCli()
-    {
-        $em = DoctrineBootstrap::getEntityManager();
-        $em->getConnection()
-            ->getDatabasePlatform()
-            ->registerDoctrineTypeMapping('enum', 'string');
-        return ConsoleRunner::createHelperSet($em);
-    }
+class DoctrineBootstrap {
+	
+	/**
+	 * entity manager
+	 *
+	 * @var _entityManager EnitytManager
+	 */
+	private static $_entityManager;
+	
+	/**
+	 * loading envs
+	 */
+	private static function _loadEnv() {
+		if (getenv ( 'DB_NAME' ) != null && getenv ( 'DB_NAME' ) != "") {
+			return;
+		}
+		if (defined ( 'ABSPATH' )) {
+			\Dotenv::load ( ABSPATH );
+		} else {
+			\Dotenv::load ( getenv ( 'HOME' ) );
+		}
+	}
+	private static function getPaths() {
+		if (file_exists ( getcwd () . "/src/Entities" )) {
+			return array (
+					getcwd () . "/src/Entities" 
+			);
+		} else {
+			return array (
+					getcwd () . "/vendor/amarcinkowski/hospitalplugin/src/Entities" 
+			);
+		}
+	}
+	
+	/**
+	 *
+	 * @return entityManager EntityManager
+	 */
+	private static function _getInstance() {
+		$isDevMode = true;
+		$config = Setup::createAnnotationMetadataConfiguration ( self::getPaths (), $isDevMode );
+		self::_loadEnv ();
+		$conn = array (
+				'dbname' => getenv ( 'DB_NAME' ),
+				'user' => getenv ( 'DB_USER' ),
+				'password' => getenv ( 'DB_PASSWORD' ),
+				'host' => getenv ( 'DB_HOST' ),
+				'driver' => 'pdo_mysql',
+				'charset' => 'utf8',
+				'driverOptions' => array (
+						1002 => 'SET NAMES utf8' 
+				),
+				'mapping_types' => 'enum:string' 
+		);
+		return EntityManager::create ( $conn, $config );
+	}
+	
+	/**
+	 * returns enitytManager
+	 *
+	 * @return EntityManager
+	 */
+	public static function getEntityManager() {
+		if (self::$_entityManager == null) {
+			self::$_entityManager = self::_getInstance ();
+		}
+		return self::$_entityManager;
+	}
+	public static function getCli() {
+		$em = DoctrineBootstrap::getEntityManager ();
+		$em->getConnection ()->getDatabasePlatform ()->registerDoctrineTypeMapping ( 'enum', 'string' );
+		return ConsoleRunner::createHelperSet ( $em );
+	}
 }
 
